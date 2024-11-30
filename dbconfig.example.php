563ae8607a7aaa48d515d6ba16006c7a141d3e3c
trafficinc <einnor212@gmail.com>
2024-11-30 14:29:39 -0500

    init

---
+++ dbconfig.example.php
@@ -0,0 +1,6 @@
+<?php
+    $host = 'localhost';
+	$db   = 'mydatabase';
+	$user = 'root';
+	$pass = 'password';
+	$charset = 'utf8mb4';
---
+++ libs/Database.php
@@ -0,0 +1,88 @@
+<?php
+
+
+class Database {
+
+	private $conn;
+	private $error;
+	private $stmt;
+
+	
+	public function __construct() {
+		include_once('dbconfig.php');
+		$dsn = 'mysql:host=' . $host . ';dbname=' . $db;
+
+		$options = array (
+			PDO::ATTR_PERSISTENT => true,
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION 
+		);
+
+		try {
+			$this->conn = new PDO ($dsn, $user, $pass, $options);
+		}	
+		catch ( PDOException $e ) {
+			$this->error = $e->getMessage();
+			// print_r( $e );
+		}
+	}
+
+	public function query($query) {
+		$this->stmt = $this->conn->query($query);
+	}
+
+		// Prepare statement with query
+	public function queryPrepare($query) {
+		$this->stmt = $this->conn->prepare($query);
+	}
+	
+	// Bind values
+	public function bind($param, $value, $type = null) {
+		if (is_null ($type)) {
+			switch (true) {
+				case is_int ($value) :
+					$type = PDO::PARAM_INT;
+					break;
+				case is_bool ($value) :
+					$type = PDO::PARAM_BOOL;
+					break;
+				case is_null ($value) :
+					$type = PDO::PARAM_NULL;
+					break;
+				default :
+					$type = PDO::PARAM_STR;
+			}
+		}
+		$this->stmt->bindValue($param, $value, $type);
+	}
+	
+	// Execute the prepared statement
+	public function execute(){
+		return $this->stmt->execute();
+	}
+
+	public function exec($sql){
+		return $this->conn->exec($sql);
+	}
+	
+	// Get result set as array of objects
+	public function resultset(){
+		$this->execute();
+		return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+	}
+	
+	// Get single record as object
+	public function single(){
+		$this->execute();
+		return $this->stmt->fetch(PDO::FETCH_OBJ);
+	}
+	
+	// Get record row count
+	public function rowCount(){
+		return $this->stmt->rowCount();
+	}
+	
+	// Returns the last inserted ID
+	public function lastInsertId(){
+		return $this->conn->lastInsertId();
+	}
+}
